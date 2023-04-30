7718f64f9d11ca727f5f06bf1510de671b72a2fe
Steven Saus <steven@stevesaus.com>
2023-04-30 19:09:09 -0400

    Got the warnings in...

--- sysmon_script.sh
+++ sysmon_script.sh
@@ -8,23 +8,27 @@
 # setting some defaults. Change as appropriate.
 WARN=80
 ALARM=95
-CPUWARN=$WARN
-CPUALARM=$ALARM
-MEMWARN=$WARN
-MEMALARM=$ALARM
-TEMPWARN=$WARN
-TEMPALARM=$ALARM
-LOADWARN=3
-LOADALARM=5
+CPU_WARN=$WARN
+CPU_ALARM=$ALARM
+MEM_WARN=$WARN
+MEM_ALARM=$ALARM
+TEMP_WARN=$WARN
+TEMP_ALARM=$ALARM
+BATTERY_WARN=$WARN
+BATTERY_ALARM=$ALARM
+LOAD_WARN=4
+LOAD_ALARM=5
 
 # setting icons
-#cpu=☢💻
-#mem=✇📀💿
-#temp=☮  🌡
-#power=⚡
-#load=⚙📜
+# first is for the plugin, rest are inline (and hence emojis)
+ICON=utilities-system-monitor
+cpu_icon=💻
+mem_icon=🧠
+temp_icon=🌡
+power_icon=🔋
+load_icon=📜
 #barometers=⚗
-#🔋🐧💾📩💥⚠
+#🐧💾📩💥⚠⚙☮✇📀💿  
 
 # Resetting variables
 CPU=""
@@ -33,6 +37,7 @@ FREEMEM=""
 TEMP=""
 LOAD=""
 IOWAIT=""
+BATTERY=""
 
 warn_colors (){
     # pass in VALUE, WARN, ALARM values, returns color
@@ -50,16 +55,17 @@ warn_colors (){
 get_cpu (){
     #https://www.baeldung.com/linux/get-cpu-usage
     CPU=$(cat /proc/stat |grep cpu |tail -1|awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'|awk '{print 100-$1}')
+    color=$(warn_colors $CPU $CPU_WARN $CPU_ALARM)
+    printf "%s <span fgcolor='%s'>%s</span>" "${cpu_icon}" "${color}" "${CPU}"
 }
 
 get_memory (){
     # get % memory used
     MEMTOT=$(cat /proc/meminfo | grep MemTotal | awk '{printf ("% 0.1f", $2/1024000)}')
     MEMAVA=$(cat /proc/meminfo | grep MemAvailable | awk '{printf ("%0.1f", $2/1024000)}')
-    MEMUSED=$(echo "scale=2;($MEMTOT - $MEMAVA)" | bc)
-
-    # This is amount of memory used
-    MEM=$(free | grep Mem | awk '{printf("%02d",  $3/$2 * 100.0)}')
+    MEM=$(echo "scale=2;($MEMTOT - $MEMAVA)" | bc)
+    color=$(warn_colors $MEM $MEM_WARN $MEM_ALARM)
+    printf "%s <span fgcolor='%s'>%s</span>" "${mem_icon}" "${color}" "${MEM}"
 }
 
 get_temp (){
@@ -68,19 +74,39 @@ get_temp (){
     #TEMP=$(sensors | awk '/[Cc]ore 0/{print $3}')
     # PACKAGE 0
     TEMP=$(sensors | awk '/Package id 0/{print $4}')
+    color=$(warn_colors $TEMP $TEMP_WARN $TEMP_ALARM)
+    printf "%s <span fgcolor='%s'>%s</span>" "${temp_icon}" "${color}" "${TEMP}"    
 }
 
 get_load (){
     LOAD=$(uptime | tr -s " " | cut -d' ' -f9- | tr -d ",")
     # iowait
     IOWAIT=$(/usr/bin/iostat -c -k -z | head -4 | tail -1 | awk '{print $4}')
+    LOAD1=$(echo $LOAD | awk '{print $1}')
+    # only really want 1m load for coloration
+    color=$(warn_colors $LOAD1 $LOAD_WARN $LOAD_ALARM)
+    printf "%s <span fgcolor='%s'>%s</span>" "${load_icon}" "${color}" "${LOAD}"        
 }
 
+get_battery (){
+    BATTERY=$(acpi -b |awk -F ": " '{print $2}'| awk -F "," '{print substr($1,1,1) $2}')
+    BATTERY_VALUE=$(acpi -b |awk -F ": " '{print $2}'| awk -F " " '{print $2}' | tr -d "%" )
+    BATTERY_WARN=$(echo "scale=2;(100-$BATTERY_WARN)" | bc)
+    BATTERY_ALARM=$(echo "scale=2;(100-$BATTERY_ALARM)" | bc) 
+    color=$(warn_colors $BATTERY_VALUE $BATTERY_WARN $BATTERY_ALARM)
+    printf "%s <span fgcolor='%s'>%s</span>" "${battery_icon}" "${color}" "${BATTERY}"        
+}
 
 do_genmon (){
     # do the genmon
     echo "<icon>$ICON</icon><iconclick>xfce4-taskmanager</iconclick>"
-    echo "<txt> $CPU | $MEMUSAGE | $HD </txt><txtclick>xfce4-taskmanager</txtclick>"
+    # build the text string
+    printf "<txt>
+    echo "<txt> $CPU | $MEMUSAGE | $HD </txt>
+    
+    <txtclick>xfce4-taskmanager</txtclick>"
+
+
     echo "<tool>-=CPU $CPULOAD=-
     $TOPCPU
 
@@ -93,10 +119,5 @@ do_genmon (){
     exit 0    
 }
 
-
-get_cpu
-get_memory
-get_load
-get_temp
 
 
